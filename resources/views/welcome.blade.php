e418652eaf622293c0e2c110c1a92d12bd0d9861
devwithu <devwithu@naver.com>
2023-01-05 09:56:53 +0900

    190 Project Category CRUD Part 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,19 @@
                     </div>
                 </div>
             </li>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsCategory"
+                   aria-expanded="true" aria-controls="collapsCategory">
+                    <i class="far fa-fw fa-window-maximize"></i>
+                    <span>Category</span>
+                </a>
+                <div id="collapsCategory" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <router-link class="collapse-item" to="/store-category">Add Category</router-link>
+                        <router-link class="collapse-item" to="/category">All Category</router-link>
+                    </div>
+                </div>
+            </li>
             <hr class="sidebar-divider">
             <div class="sidebar-heading">
                 Examples
